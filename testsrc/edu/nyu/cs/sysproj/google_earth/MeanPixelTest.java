e969fbfbc11b487a1eb72e66d3f8ec1c724992e3
Scot Dalton <scotdalton@gmail.com>
2011-11-03 18:29:00 -0400

    Cleanup

---
+++ testsrc/edu/nyu/cs/sysproj/google_earth/MeanPixelTest.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package edu.nyu.cs.sysproj.google_earth;
+
+import static edu.nyu.cs.sysproj.google_earth.TestUtility.getArableTrainingImageFiles;
+
+import java.io.File;
+import java.util.List;
+
+import org.junit.Test;
+
+import edu.nyu.cs.sysproj.google_earth.features.MeanPixel;
+
+
+
+/**
+ * @author Scot Dalton
+ *
+ */
+public class MeanPixelTest {
+
+	@Test
+	public void testMeanRed() {
+		for(File file : getArableTrainingImageFiles()) {
+			Image image = new Image(file);
+			List<Image> choppedImages = image.getChoppedImages();
+			for(Image choppedImage : choppedImages) {
+				MeanPixel meanPixelBand1 = 
+					new MeanPixel(choppedImage, 0);
+				System.out.println("File: " + file.getName());
+				System.out.println("\tMean pixel band 1: " + meanPixelBand1.getValue());
+			}
+		}
+	}
+
+	@Test
+	public void testMeanGreen() {
+		for(File file : getArableTrainingImageFiles()) {
+			Image image = new Image(file);
+			List<Image> choppedImages = image.getChoppedImages();
+			for(Image choppedImage : choppedImages) {
+				MeanPixel meanColorBand2 = 
+					new MeanPixel(choppedImage, 1);
+				System.out.println("File: " + file.getName());
+				System.out.println("\tMean green pixel: " + meanColorBand2.getValue());
+			}
+		}
+	}
+
+	@Test
+	public void testMeanBlue() {
+		for(File file : getArableTrainingImageFiles()) {
+			Image image = new Image(file);
+			List<Image> choppedImages = image.getChoppedImages();
+			for(Image choppedImage : choppedImages) {
+				MeanPixel meanColorBand3 = 
+					new MeanPixel(choppedImage, 2);
+				System.out.println("File: " + file.getName());
+				System.out.println("\tMean blue pixel: " + meanColorBand3.getValue());
+			}
+		}
+	}
+}
