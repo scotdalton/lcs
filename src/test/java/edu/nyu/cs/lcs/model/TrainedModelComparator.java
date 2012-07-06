8822130ecb540c88e5ace874d341871e32683404
Scot Dalton <scotdalton@gmail.com>
2012-07-06 15:37:12 -0400

    Refactor for Fiji.

--- src/test/java/edu/nyu/cs/lcs/model/TrainedModelComparator.java
+++ src/test/java/edu/nyu/cs/lcs/model/TrainedModelComparator.java
@@ -10,61 +10,77 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import org.yaml.snakeyaml.Yaml;
-
-import com.google.common.collect.Lists;
 
 import weka.classifiers.AbstractClassifier;
 import weka.classifiers.Classifier;
 import weka.core.Utils;
+
+import com.google.common.collect.Lists;
+import com.google.common.io.Files;
 
 /**
  * @author Scot Dalton
  * 
  * */
 public class TrainedModelComparator {
-	private static final File classifiersFile = 
+	private File classifiersFile = 
 		new File("src/test/resources/META-INF/classifiers.yml");
-	private static final File featuresFile = 
+	private File featuresFile = 
 		new File("src/test/resources/META-INF/features.yml");
-	private static final File serializationDirectory = new File(".classifiers/default");
+	private File serializationDirectory = new File(".classifiers/default_features");
+	private String encoding = "UTF-8";
+	private File comparisons = new File("comparisons");
 
 	@Test
 	@SuppressWarnings("unchecked")
 	public void comparator() throws Exception {
+		boolean quit = false;
 		Yaml yaml = new Yaml();
 		List<String> features = 
 			(List<String>) yaml.load(new FileReader(featuresFile));
 		Map<String, List<String>> classifiers = 
 			(Map<String, List<String>>) yaml.load(new FileReader(classifiersFile));
+		Files.write("".getBytes(), comparisons);
+		List<String> lines = Lists.newArrayList();
 		for (Entry<String, List<String>> entry: classifiers.entrySet()) {
 			String classifierName = entry.getKey();
 			List<String> classifierOptions = Lists.newArrayList();
 			if (entry.getValue() != null) 
 				classifierOptions = entry.getValue();
-			System.out.println("---");
-			System.out.println(classifierName);
+			lines.add("---");
+			lines.add(classifierName);
 			try {
-				AbstractClassifier classifier = 
-					(AbstractClassifier) Utils.forName(Classifier.class, classifierName, classifierOptions.toArray(new String[0]));
+				TrainedModel trainedModel;
 				long startGet = Calendar.getInstance().getTimeInMillis();
 				if (serializationDirectory.exists() || serializationDirectory.mkdirs()) {
-					TrainedModel trainedModel = 
-						new TrainedModel(serializationDirectory, classifier);
+					if (classifierName.equals("default")) {
+						trainedModel = new TrainedModel(serializationDirectory);
+					} else {
+						AbstractClassifier classifier = 
+							(AbstractClassifier) Utils.forName(Classifier.class, classifierName, classifierOptions.toArray(new String[0]));
+	 					trainedModel = 
+							new TrainedModel(serializationDirectory, classifier);
+					}
 					long endGet = Calendar.getInstance().getTimeInMillis();
 					long startTest = Calendar.getInstance().getTimeInMillis();
-					System.out.println(trainedModel.test());
+					lines.add(trainedModel.test());
 					long endTest = Calendar.getInstance().getTimeInMillis();
-					System.out.println("Time to train: " + (endGet- startGet)/(double)1000);
-					System.out.println("Time to test: " + (endTest- startTest)/(double)1000);
+					lines.add("Time to train: " + (endGet- startGet)/(double)1000);
+					lines.add("Time to test: " + (endTest- startTest)/(double)1000);
 				} else {
+					quit = true;
 					throw new Exception("Problems with the serialization directory");
 				}
-			} catch (RuntimeException e) {
+			} catch (Exception e) {
 				e.printStackTrace();
+				lines.add(e.getMessage());
 			}
-			System.out.println();
+			lines.add(null);
+			FileUtils.writeLines(comparisons, encoding, lines, true);
+			if (quit) System.exit(1);
 		}
 	}
 }
