c88fd38b3f722aec0c1358f0b9db2f9a80ba137a
Scot Dalton <scotdalton@gmail.com>
2012-09-10 10:15:08 -0400

    Start with 2000th image for parallel processing

--- src/test/java/edu/nyu/cs/lcs/ImageClassifierTest.java
+++ src/test/java/edu/nyu/cs/lcs/ImageClassifierTest.java
@@ -43,8 +43,8 @@ public class ImageClassifierTest {
 	@Test
 	public void westBengal2000() throws Exception {
 		File wb = new File(wbBase + "/2000-05-21");
-		File wbCsv = new File(wbBase + "/2000-05-21-4.csv");
-		FileUtil.regionCSV(wb, wbCsv, getTrainedModel(), 2000);
+		File wbCsv = new File(wbBase + "/2000-05-21-5.csv");
+		FileUtil.regionCSV(wb, wbCsv, getTrainedModel(), 2500);
 	}
 	
 	@Ignore
