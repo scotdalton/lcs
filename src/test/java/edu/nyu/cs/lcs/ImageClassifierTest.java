d271fb95393482b80dbb6ad9e38f8e9b187f0695
Scot Dalton <scotdalton@gmail.com>
2012-09-17 09:52:09 -0400

    Update to process West Bengal images.

--- src/test/java/edu/nyu/cs/lcs/ImageClassifierTest.java
+++ src/test/java/edu/nyu/cs/lcs/ImageClassifierTest.java
@@ -59,7 +59,7 @@ public class ImageClassifierTest {
 	public void westBengal2000() throws Exception {
 		File wb = new File(wbBase + "/2000-05-21");
 		File wbCsv = new File(wbBase + "/2000-05-21-2a.csv");
-		FileUtil.regionCSV(wb, wbCsv, getTrainedModel(), 505);
+		FileUtil.regionCSV(wb, wbCsv, getTrainedModel(), 18);
 	}
 	
 	@Ignore
